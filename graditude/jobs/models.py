bc074741a1dc0db36466237e285900ee082548a1
Tom Vo <t_vo16@u.pacific.edu>
2019-08-27 21:46:09 -0700

    Initial commit

---
+++ graditude/jobs/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Company(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Source(models.Model):
+    NAME_CHOICES = (('Indeed', 'Indeed'),
+                    ('LinkedIn', 'LinkedIn'),
+                    ('Monster', 'Monster'))
+    name = models.CharField(max_length=255, choices=NAME_CHOICES)
+    using_api = models.BooleanField()
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+    description = models.TextField()
+    posted_on = models.DateField()
+    added = models.DateField(auto_created=True)
+    sponsored = models.BooleanField()
+    source = models.ForeignKey('Source', on_delete=models.CASCADE)
+    link = models.URLField()
