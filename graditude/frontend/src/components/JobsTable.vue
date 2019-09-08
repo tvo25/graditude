a69b993bf6b0e19cda8beb7b8b45c5dd0558c63b
Tom Vo <t_vo16@u.pacific.edu>
2019-09-07 20:41:51 -0700

    Update JobsTable component
    - Closes #22
    - Switch to Buefy Table component
    - Render link field from API as raw 'a' tag HTML link
    - Add pagination, fixed width columns and loading bar while fetching from the database

--- graditude/frontend/src/components/JobsTable.vue
+++ graditude/frontend/src/components/JobsTable.vue
@@ -1,16 +1,52 @@
 <template>
     <div id="JobsTable">
-        <v-data-table
-                dense
-                calculate-widths
-                fixed-header
-                :headers="headers"
-                :items="posts"
-                :items-per-page="20"
-                class="elevation-1"
-                :loading="loading"
-                loading-text="Loading job listings, please wait..."
-        ></v-data-table>
+        <b-field grouped group-multiline>
+            <b-select v-model="perPage" :disabled="!isPaginated">
+                <option value="5">5 per page</option>
+                <option value="10">10 per page</option>
+                <option value="15">15 per page</option>
+                <option value="20">20 per page</option>
+            </b-select>
+        </b-field>
+        <b-table :data="data"
+                 :loading="loading"
+                 
+                 :paginated="isPaginated"
+                 :per-page="perPage"
+                 :current-page.sync="currentPage"
+                 :pagination-simple="isPaginationSimple"
+                 :pagination-position="paginationPosition"
+
+                 backend-sorting
+                 :default-sort-direction="defaultSortOrder"
+                 :default-sort="[sortField, sortOrder]"
+                 @sort="onSort"
+        >
+
+
+            <template slot-scope="props">
+                <b-table-column field="date_posted" label="Date Posted" sortable centered>
+                    {{ props.row.date_posted ? new Date(props.row.date_posted).toLocaleDateString() : '' }}
+                </b-table-column>
+
+                <b-table-column field="title" label="Title" width="300" sortable>
+                    {{ props.row.title }}
+                </b-table-column>
+
+                <b-table-column field="company" label="Company" width="150" sortable>
+                    {{ props.row.company }}
+                </b-table-column>
+
+                <b-table-column field="description" label="Description" sortable>
+                    {{ props.row.description }}
+                </b-table-column>
+
+                <b-table-column field='link' label="Source" width="100">
+                    <p v-html="_linkify(props.row.link)"></p>
+                </b-table-column>
+            </template>
+
+        </b-table>
     </div>
 </template>
 
@@ -21,46 +57,44 @@
         name: "JobsTable",
         data() {
             return {
-                headers: [
-                    {
-                        text: 'Title', value: 'title'
-                    },
-                    {
-                        text: 'Company', value: 'company'
-                    },
-                    {
-                        text: 'Description', value: 'description'
-                    },
-                    {
-                        text: 'Date Posted', value: 'date_posted'
-                    },
-                    {
-                        text: 'Link', value: 'link'
-                    }
-                ],
-                posts: [],
-                loading: true
+                data: [],
+                loading: true,
+                isPaginated: true,
+                isPaginationSimple: false,
+                paginationPosition: 'bottom',
+                currentPage: 1,
+                perPage: 5
+
 
             }
         },
         methods: {
-            getAllPosts: function () {
+            getAllData: function () {
                 axios
                     .get('/jobs/api/v1/posts/')
                     .then(response => {
-                        this.posts = response.data
+                        this.data = response.data
                         this.loading = false
 
                     })
                     .catch(error => {
-                        console.log(error)
+                        this.data = []
                         this.errored = true
+                        this.loading = false
+
                     })
                     .finally(() => this.loading = false)
+            },
+            /**
+             * Converts the raw link text from the posts API to HTML
+             * @param {String} text The raw link text
+             * */
+            _linkify: function (text) {
+                return text.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">Link</a>');
             }
         },
         mounted() {
-            this.getAllPosts()
+            this.getAllData()
 
         }
     };
