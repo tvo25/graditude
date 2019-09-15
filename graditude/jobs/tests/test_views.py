c688f38b703ba037f830a3a6be40fef0c0c61df0
Tom Vo <t_vo16@u.pacific.edu>
2019-09-15 15:32:31 -0700

    Update unit test for views for Jobs API
    - See #16
    - Update pytest settings to not reuse the same test database, which was causing issues with Factory Boy duplicate insertions
    - Update .gitignore to ignore the new directory for coverage generated HTML reports
    - Update .coveragerc

--- graditude/jobs/tests/test_views.py
+++ graditude/jobs/tests/test_views.py
@@ -4,6 +4,7 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from graditude.jobs import models
 from graditude.jobs.tests import factories
 
 if TYPE_CHECKING:
@@ -16,22 +17,22 @@ class TestJobsAPI:
     @pytest.fixture(autouse=True)
     def setUp(self, client: 'Client'):
         self.client = client
-        self.post = factories.PostFactory()
-        self.list_url = reverse('jobs:list')
-        self.detail_url = reverse('jobs:detail', kwargs={'pk': self.post.id})
 
     def test_list(self):
+        list_url = reverse('jobs:list')
+
         for i in range(100):
             factories.PostFactory()
-        response = self.client.get(self.list_url)
+        count = models.Post.objects.count()
+
+        response = self.client.get(list_url)
 
         assert response.status_code == status.HTTP_200_OK
-
-        # Test that there are 101 items in the JSON response
-        # 100 being within this test, and the 1 extra object form the
-        # setup
-        assert len(response.data) == 101
+        assert len(response.data) == count
 
     def test_detail(self):
-        response = self.client.get(self.detail_url)
-        assert response.json().get('title') == self.post.title
+        post = factories.PostFactory()
+        detail_url = reverse('jobs:detail', kwargs={'pk': post.id})
+
+        response = self.client.get(detail_url)
+        assert response.json().get('title') == post.title
