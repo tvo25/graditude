3cd727f35745de5c6c8ddef948f7fc2a3f9ac5d5
Tom Vo <tomvothecoder@gmail.com>
2019-09-21 17:00:08 -0700

    Update font for tables

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -20,11 +20,13 @@
     h3,
     h4,
     h5,
-    h6 {
+    h6,
+    th {
         font-family: "Raleway", sans-serif;
     }
 
-    p {
+    p,
+    td {
         font-family: "Lato", sans-serif;
     }
 </style>
