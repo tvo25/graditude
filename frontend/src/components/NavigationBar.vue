f3ce5a806ab808865e1cddcce9c1e98c16e8d9ce
Tom Vo <tomvothecoder@gmail.com>
2019-11-14 13:08:56 -0800

    Add skeleton nav bar

---
+++ frontend/src/components/NavigationBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <a class="navbar-item" href="#" style="font-weight:bold;">Graditude</a>
+        <span class="navbar-burger burger" data-target="navMenu">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div id="navMenu" class="navbar-menu">
+        <div class="navbar-end">
+          <a href="#" class="navbar-item is-active">Home</a>
+          <a href="#" class="navbar-item">About Us</a>
+          <a href="#" class="navbar-item">Services</a>
+          <a href="#" class="navbar-item">Contact Us</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = Array.prototype.slice.call(
+    document.querySelectorAll(".navbar-burger"),
+    0
+  );
+
+  // Check if there are any nav burgers
+  if ($navbarBurgers.length > 0) {
+    // Add a click event on each of them
+    $navbarBurgers.forEach(function($el) {
+      $el.addEventListener("click", function() {
+        // Get the target from the "data-target" attribute
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+
+        // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
+      });
+    });
+  }
+});
+
+export default {
+  name: "NavigationBar"
+};
+</script>
+
+<style>
+</style>
