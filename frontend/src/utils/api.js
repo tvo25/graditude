b4c83e81d0133a619bb05d9f66ff60818d7d49ef
Tom Vo <tomvothecoder@gmail.com>
2020-04-11 20:14:34 -0700

    Add project scaffolding and base components

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,1 @@
+// A placeholder file for future API based functions
---
+++ frontend/src/views/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
+import Hero from '../components/Hero';
+
+function Home() {
+  return (
+    <div>
+      <NavBar />
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
