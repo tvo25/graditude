fb64b5a386eb1f1358616468c0b098e700f1b06c
tomvothecoder <tomvothecoder@gmail.com>
2020-04-12 15:50:43 -0700

    Add hero content

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Row, Col, Button } from 'antd';
+
+const styles = {
+  container: {
+    height: '544px',
+    background:
+      'linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(28, 93, 132, 0.06993090204831931) 100%)',
+  },
+  heroTitle: {
+    fontSize: '48px',
+    fontWeight: 'bold',
+  },
+};
 
 function Hero() {
-  return <div></div>;
+  return (
+    <Row style={styles.container} type="flex" align="middle">
+      <Col span={6} offset={9}>
+        <h1 style={styles.heroTitle}>
+          It only takes a little bit of Graditude.
+        </h1>
+        <p>Graditude aims to optimize your job hunt with just a few clicks.</p>
+
+        <Button type="primary">Ready to elevate your search?</Button>
+      </Col>
+    </Row>
+  );
 }
 
 export default Hero;
