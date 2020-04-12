14f3e251b66cbcf99057dd682550933ec290886b
tomvothecoder <tomvothecoder@gmail.com>
2020-04-12 16:27:05 -0700

    Add promo section - Fix static sized hero title

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,7 +8,7 @@ const styles = {
       'linear-gradient(180deg, rgba(255, 255, 255, 1) 0%, rgba(28, 93, 132, 0.06993090204831931) 100%)',
   },
   heroTitle: {
-    fontSize: '48px',
+    fontSize: '2.5rem',
     fontWeight: 'bold',
   },
 };
@@ -16,7 +16,7 @@ const styles = {
 function Hero() {
   return (
     <Row style={styles.container} type="flex" align="middle">
-      <Col span={6} offset={9}>
+      <Col span={10} offset={6}>
         <h1 style={styles.heroTitle}>
           It only takes a little bit of Graditude.
         </h1>
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
+import { Row, Col, Typography } from 'antd';
 import Hero from '../components/Hero';
+
+import teamImg from '../assets/img/teamwork.svg';
+import cloudImg from '../assets/img/cloud-computing.svg';
+import yogaImg from '../assets/img/yoga.svg';
+
+const styles = {
+  img: { height: '128px', width: '128px' },
+  promoCaption: { fontWeight: 'bold', fontSize: '14px' },
+};
 
 function Home() {
   return (
     <div>
-      <Hero />
+      <div>
+        <Hero />
+      </div>
+      <div style={styles.promoContainer}>
+        <Row type="flex" align="middle">
+          <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+            <img style={styles.img} src={teamImg} alt="team"></img>
+            <p style={styles.promoCaption}>Stay Motivated</p>
+            <p>
+              Job hunting for new graduates can be tough. Graditude&apos;s
+              mission is to encourage the growth of the next generation of
+              professionals.
+            </p>
+          </Col>
+          <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+            <img style={styles.img} src={cloudImg} alt="cloud"></img>
+            <p style={styles.promoCaption}>Searching, made simple</p>
+            <p>
+              Browse a simple, centralized database curated using Indeed.
+              Graditude&apos;s listings are updated twice an hour, every hour.
+            </p>
+          </Col>
+          <Col xs={24} sm={24} md={8} lg={8} xl={8}>
+            <img style={styles.img} src={yogaImg} alt="stress-free"></img>
+            <p style={styles.promoCaption}>Ad-free, stress-free</p>
+            <p>
+              Graditude makes job-hunting stress-free by filtering out ads and
+              displaying posts tabularly. The fewer clicks are made, the more
+              efficient searching becomes.
+            </p>
+          </Col>
+        </Row>
+      </div>
     </div>
   );
 }
